64111e5f465c3e18d87ce3c3434fe5f7b11c9d97
zw277 <112585430+zw277@users.noreply.github.com>
2022-11-27 04:01:30 +0000

    update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import uvicorn
 from mylibrary.logic import search_wiki as wikisearch
 from mylibrary.logic import wiki as wikiintro
 from mylibrary.logic import phrase as wikiphrases
+from mylibrary.logic import search_pub
+from mylibrary.logic import fetch_details
 
 app = FastAPI()
 
@@ -31,6 +33,20 @@ async def phrase(name: str):
     """Return related phrases at wikipedia"""
 
     result = wikiphrases(name)
+    return {"result": result}
+
+@app.get("/search_pub/{name}")
+async def search_pub(name: str):
+    """Return related phrases at PubMed"""
+
+    result = search_pub(name)
+    return {"result": result}
+
+@app.get("/fetch_details/{name}")
+async def fetch_details(name: str):
+    """Return related phrases at PubMed"""
+
+    result = fetch_details(name)
     return {"result": result}
 
 if __name__ == "__main__":
--- mylibrary/logic.py
+++ mylibrary/logic.py
@@ -1,5 +1,6 @@
 import wikipedia
 from textblob import TextBlob
+from Bio import Entrez
 
 def wiki(name="Duke University", length=3):
     """This is a wikipedia fetcher for the basic information of reseach"""
@@ -19,3 +20,23 @@ def phrase(name):
     page = wiki(name)
     blob = TextBlob(page)
     return blob.noun_phrases
+
+
+def search_pub(query):
+    Entrez.email = 'your.email@example.com'
+    handle = Entrez.esearch(db='pubmed', 
+                            sort='relevance', 
+                            retmax='20',
+                            retmode='xml', 
+                            term=query)
+    results1 = Entrez.read(handle)
+    return results1
+
+def fetch_details(id_list):
+    ids = ','.join(id_list)
+    Entrez.email = 'your.email@example.com'
+    handle = Entrez.efetch(db='pubmed',
+                           retmode='xml',
+                           id=ids)
+    results2 = Entrez.read(handle)
+    return results2
