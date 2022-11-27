5001740b640cc8b0a14ca8fdd5d58030815a6b44
zw277 <112585430+zw277@users.noreply.github.com>
2022-11-27 06:20:27 +0000

    update

--- main.py
+++ main.py
@@ -8,7 +8,28 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {
-        "message": "This is a Wikipedia API. Please call: /search or /wiki or /phrase"
+        "message": "This is a Arxiv API. Call: /docs or /arx/{categories you search}",
+        "Computer Science": "cs",
+        "Economics": "econ",
+        "Electrical Engineering and Systems Science":"eess",
+        "Mathematics":"math",
+        "Physics":	"physics",
+        "Astrophysics":	"physics:astro-ph",
+        "Condensed Matter":	"physics:cond-mat",
+        "General Relativity and Quantum Cosmology":	"physics:gr-qc",
+        "High Energy Physics - Experiment":	"physics:hep-ex",
+        "High Energy Physics - Lattice":"physics:hep-lat",
+        "High Energy Physics - Phenomenology":"physics:hep-ph",
+        "High Energy Physics - Theory":"physics:hep-th",
+        "Mathematical Physics":"physics:math-ph",
+        "Nonlinear Sciences":"physics:nlin",
+        "Nuclear Experiment":"physics:nucl-ex",
+        "Nuclear Theory":"physics:nucl-th",
+        "Physics (Other)":"physics:physics",
+        "Quantum Physics":"physics:quant-ph",
+        "Quantitative Biology":"q-bio",
+        "Quantitative Finance":"q-fin",
+        "Statistics":"stat"
     }
 
 @app.get("/arx/{name}")
